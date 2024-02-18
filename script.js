e4b1553fac8f590de889ea142d6e91727d969f22
AjajRana <aj.rajput0003@gmail.com>
2024-02-18 19:45:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var menuIcon = document.querySelector(".menu_icon");
+var sideBar = document.querySelector(".sidebar");
+var container = document.querySelector(".container")
+menuIcon.onclick = function () {
+    sideBar.classList.toggle("small_sidebar");
+    container.classList.toggle("large_container")
+}
